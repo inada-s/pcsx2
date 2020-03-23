6469195dc936628a6c500c9cbeed9307c3370717
Shingo INADA <mecha.is06@gmail.com>
2020-03-24 03:54:53 +0900

    bypass tcp

--- pcsx2/gui/Debugger/GundamDXDebug.cpp
+++ pcsx2/gui/Debugger/GundamDXDebug.cpp
@@ -10,6 +10,8 @@
 #include "DebugTools/DebugInterface.h"
 #include "DisassemblyDialog.h"
 #include "Dump.h"
+
+
 //#include "CtrlDisassemblyView.h"
 //#include "CtrlRegisterList.h"
 //#include "CtrlMemView.h"
@@ -19,103 +21,45 @@
 namespace
 {
 
-static std::map<std::string, std::function<void(bool, DebugInterface *)>> gdx_remaps_name;
-static std::map<u32, std::function<void(bool, DebugInterface *)>> gdx_remaps_addr;
-static std::map<std::string, SymbolEntry> gdx_remaps_symbols;
-static std::map<u32, SymbolEntry> addr2symbol;
+std::map<std::string, std::function<void(bool, DebugInterface *)>> gdx_remaps_name;
+std::map<u32, std::function<void(bool, DebugInterface *)>> gdx_remaps_addr;
+std::map<std::string, SymbolEntry> gdx_remaps_symbols;
+std::map<u32, SymbolEntry> addr2symbol;
 
-static const u32 OP_NOP = 0;
-static const u32 OP_JR_RA = 0x03e00008;
+const u32 OP_NOP = 0;
+const u32 OP_JR_RA = 0x03e00008;
 
-/*
-const tMipsRegister MipsRegister[] = {
-	{ "r0", 0, 2 }, { "zero", 0, 4}, { "$0", 0, 2 }, { "$zero", 0, 5 },
-	{ "at", 1, 2 }, { "r1", 1, 2 }, { "$1", 1, 2 }, { "$at", 1, 3 },
-	{ "v0", 2, 2 }, { "r2", 2, 2 }, { "$v0", 2, 3 },
-	{ "v1", 3, 2 }, { "r3", 3, 2 }, { "$v1", 3, 3 },
-	{ "a0", 4, 2 }, { "r4", 4, 2 }, { "$a0", 4, 3 },
-	{ "a1", 5, 2 }, { "r5", 5, 2 }, { "$a1", 5, 3 },
-	{ "a2", 6, 2 }, { "r6", 6, 2 }, { "$a2", 6, 3 },
-	{ "a3", 7, 2 }, { "r7", 7, 2 }, { "$a3", 7, 3 },
-	{ "t0", 8, 2 }, { "r8", 8, 2 }, { "$t0", 8, 3 },
-	{ "t1", 9, 2 }, { "r9", 9, 2 }, { "$t1", 9, 3 },
-	{ "t2", 10, 2 }, { "r10", 10, 3 }, { "$t2", 10, 3 },
-	{ "t3", 11, 2 }, { "r11", 11, 3 }, { "$t3", 11, 3 },
-	{ "t4", 12, 2 }, { "r12", 12, 3 }, { "$t4", 12, 3 },
-	{ "t5", 13, 2 }, { "r13", 13, 3 }, { "$t5", 13, 3 },
-	{ "t6", 14, 2 }, { "r14", 14, 3 }, { "$t6", 14, 3 },
-	{ "t7", 15, 2 }, { "r15", 15, 3 }, { "$t7", 15, 3 },
-	{ "s0", 16, 2 }, { "r16", 16, 3 }, { "$s0", 16, 3 },
-	{ "s1", 17, 2 }, { "r17", 17, 3 }, { "$s1", 17, 3 },
-	{ "s2", 18, 2 }, { "r18", 18, 3 }, { "$s2", 18, 3 },
-	{ "s3", 19, 2 }, { "r19", 19, 3 }, { "$s3", 19, 3 },
-	{ "s4", 20, 2 }, { "r20", 20, 3 }, { "$s4", 20, 3 },
-	{ "s5", 21, 2 }, { "r21", 21, 3 }, { "$s5", 21, 3 },
-	{ "s6", 22, 2 }, { "r22", 22, 3 }, { "$s6", 22, 3 },
-	{ "s7", 23, 2 }, { "r23", 23, 3 }, { "$s7", 23, 3 },
-	{ "t8", 24, 2 }, { "r24", 24, 3 }, { "$t8", 24, 3 },
-	{ "t9", 25, 2 }, { "r25", 25, 3 }, { "$t9", 25, 3 },
-	{ "k0", 26, 2 }, { "r26", 26, 3 }, { "$k0", 26, 3 },
-	{ "k1", 27, 2 }, { "r27", 27, 3 }, { "$k1", 27, 3 },
-	{ "gp", 28, 2 }, { "r28", 28, 3 }, { "$gp", 28, 3 },
-	{ "sp", 29, 2 }, { "r29", 29, 3 }, { "$sp", 29, 3 },
-	{ "fp", 30, 2 }, { "r30", 30, 3 }, { "$fp", 30, 3 },
-	{ "ra", 31, 2 }, { "r31", 31, 3 }, { "$ra", 31, 3 },
-	{ NULL, -1, 0}
-};
-*/
+std::string last_resolved_hostname;
+
+// winsock data
+WSADATA wsa;
+SOCKET sock = 0;
+fd_set fds, readfds;
 
 enum {
-    REG_r0,
-    REG_at,
-    REG_v0,
-    REG_v1,
-    REG_a0,
-    REG_a1,
-    REG_a2,
-    REG_a3,
-    REG_t0,
-    REG_t1,
-    REG_t2,
-    REG_t3,
-    REG_t4,
-    REG_t5,
-    REG_t6,
-    REG_t7,
-    REG_s0,
-    REG_s1,
-    REG_s2,
-    REG_s3,
-    REG_s4,
-    REG_s5,
-    REG_s6,
-    REG_s7,
-    REG_t8,
-    REG_t9,
-    REG_k0,
-    REG_k1,
-    REG_gp,
-    REG_sp,
-    REG_fp,
-    REG_ra,
+    REG_r0, REG_at, REG_v0, REG_v1, REG_a0, REG_a1, REG_a2, REG_a3,
+    REG_t0, REG_t1, REG_t2, REG_t3, REG_t4, REG_t5, REG_t6, REG_t7,
+    REG_s0, REG_s1, REG_s2, REG_s3, REG_s4, REG_s5, REG_s6, REG_s7,
+    REG_t8, REG_t9, REG_k0, REG_k1, REG_gp, REG_sp, REG_fp, REG_ra,
 };
 
-void modem_module_initialize(bool init, DebugInterface *cpu)
+std::string gdx_read_string(DebugInterface *cpu, u32 address)
 {
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
+    if (address == 0) {
+        return "";
     }
 
-    cpu->setRegister(0, REG_v0, (u128::From32(0)));
+    char buf[4096] = {0};
+    for (int i = 0; i < 4096; ++i) {
+        buf[i] = static_cast<char>(cpu->read8(address + i));
+        if (buf[i] == 0) {
+            break;
+        }
+    }
+
+    return std::string(buf);
 }
+
 /*
 Ave_SifCallRpc
 Ave_TcpInitialize
@@ -156,68 +100,10 @@ void Ave_SifCallRpc(bool init, DebugInterface *cpu)
         cpu->write32(func.address, OP_JR_RA);
         return;
     }
+
+	assert(false);
 }
 
-
-void Ave_TcpInitialize(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-void Ave_TcpTerminate(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-void Ave_SetOpt(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-void Ave_GetOpt(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
 
 void Ave_TcpOpen(bool init, DebugInterface *cpu)
 {
@@ -232,6 +118,34 @@ void Ave_TcpOpen(bool init, DebugInterface *cpu)
         cpu->write32(func.address, OP_JR_RA);
         return;
     }
+
+    printf("TcpOpen:%s\n", last_resolved_hostname.c_str());
+    if (last_resolved_hostname.empty()) {
+        printf("no host available");
+        return;
+    }
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        printf("socket error : %d\n", WSAGetLastError());
+        return;
+    }
+
+	puts("connecting to dummy server");
+    // local dummy server
+    auto hostEntry = gethostbyname("localhost");
+    assert(hostEntry);
+
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr = *((LPIN_ADDR)hostEntry->h_addr_list[0]);
+    addr.sin_port = htons(3333);
+    auto err = connect(sock, (const sockaddr *)&addr, sizeof(addr));
+    assert(err == 0);
+    FD_SET(sock, &readfds);
+
+    cpu->setRegister(0, REG_v0, u128::From32(1)); // return socket number
+	puts("connecting to dummy server done");
 }
 
 void Ave_TcpClose(bool init, DebugInterface *cpu)
@@ -246,6 +160,11 @@ void Ave_TcpClose(bool init, DebugInterface *cpu)
         }
         cpu->write32(func.address, OP_JR_RA);
         return;
+    }
+
+    if (sock != 0) {
+        closesocket(sock);
+        sock = 0;
     }
 }
 
@@ -262,6 +181,26 @@ void Ave_TcpStat(bool init, DebugInterface *cpu)
         cpu->write32(func.address, OP_JR_RA);
         return;
     }
+
+    // cpu->getRegister(0, REG_a0) // the socket number?
+
+    // return value:
+    // -1 : error
+    // 0 : no data comming
+    // n : how many byts can be recv.
+    int n = 0;
+    memcpy(&fds, &readfds, sizeof(fd_set));
+    timeval timeout;
+    timeout.tv_usec = 1;
+    select(0, &fds, NULL, NULL, &timeout);
+    char buf[1024] = {0};
+    if (FD_ISSET(sock, &fds)) {
+        n = recv(sock, buf, sizeof(buf), MSG_PEEK); // peek
+        printf("recv %d bytes (peek)\n", n);
+        cpu->write32(cpu->getRegister(0, REG_a1), 4); // important value
+		// cpu->setRegister(0, REG_a1, u128::From32(4)); // important value
+    }
+	cpu->setRegister(0, REG_v0, u128::From32(n));
 }
 
 void Ave_TcpSend(bool init, DebugInterface *cpu)
@@ -277,6 +216,18 @@ void Ave_TcpSend(bool init, DebugInterface *cpu)
         cpu->write32(func.address, OP_JR_RA);
         return;
     }
+
+	// int write(sock_t sock, void* buff, const size_t len)
+    char buf[1024] = {0};
+    // int sock = cpu->getRegister(0, REG_a0)._u32[0];
+    const u32 p = cpu->getRegister(0, REG_a1)._u32[0];
+    const u32 len = cpu->getRegister(0, REG_a2)._u32[0];
+    for (int i = 0; i < len; ++i) {
+        buf[i] = cpu->read8(p + i);
+    }
+    const int n = send(sock, buf, len, 0);
+	assert(n == len);
+	cpu->setRegister(0, REG_v0, u128::From32(n));
 }
 
 void Ave_TcpRecv(bool init, DebugInterface *cpu)
@@ -292,164 +243,20 @@ void Ave_TcpRecv(bool init, DebugInterface *cpu)
         cpu->write32(func.address, OP_JR_RA);
         return;
     }
-}
 
-void Ave_PppInit(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
+	// int sock_read(sock_t sock, void* buff, const size_t len)
+    char buf[1024] = {0};
+    // int sock = cpu->getRegister(0, REG_a0)._u32[0];
+    const u32 p = cpu->getRegister(0, REG_a1)._u32[0];
+    const u32 len = cpu->getRegister(0, REG_a2)._u32[0];
+    const int n = recv(sock, buf, len, 0);
+	assert(n == len);
+    for (int i = 0; i < n; ++i) {
+        cpu->write8(p + i, buf[i]);
     }
+	cpu->setRegister(0, REG_v0, u128::From32(n));
 }
 
-void Ave_PppDisp(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-void Ave_PppStart(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-
-
-void Ave_PppFinish(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-void PppGetStatus(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-
-    cpu->setRegister(0, REG_v0, u128::From32(0)); // return status code?.
-}
-
-void Ave_PppGetUsbDeviceId(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-
-    cpu->setRegister(0, REG_v0, u128::From32(1)); // return device_id.
-}
-
-void Ave_PppSetOption(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-void Ave_PppGetOption(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-void Ave_DnsInit(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
-
-
-void Ave_DnsDisp(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-}
 
 void Ave_DnsGetTicket(bool init, DebugInterface *cpu)
 {
@@ -480,43 +287,7 @@ void Ave_DnsReleaseTicket(bool init, DebugInterface *cpu)
         return;
     }
 
-	// returns -1 means failed
-	cpu->setRegister(0, REG_v0, u128::From32(0)); // ?
-}
-
-void Ave_DnsLookUp(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-
-    puts(cpu->getRegister(EECAT_CP0, REG_a0).ToString());
-    cpu->setRegister(0, REG_v0, u128::From32(-1)); // ?
-}
-
-
-void dialing_proc(bool init, DebugInterface *cpu)
-{
-    puts(__func__);
-    auto func = gdx_remaps_symbols[__func__];
-
-    if (init) {
-        // erase the function.
-        for (int i = 0; i < func.size; ++i) {
-            cpu->write8(func.address + i, 0);
-        }
-        cpu->write32(func.address, OP_JR_RA);
-        return;
-    }
-
+    // returns -1 means failed
     cpu->setRegister(0, REG_v0, u128::From32(0)); // ?
 }
 
@@ -535,7 +306,11 @@ void gethostbyname_ps2_0(bool init, DebugInterface *cpu)
         return;
     }
 
-	cpu->setRegister(0, REG_v0, u128::From32(0)); // ?
+    auto hostname = gdx_read_string(cpu, cpu->getRegister(0, REG_a0));
+    printf("gethostbyname_ps2_0 %s\n", hostname.c_str());
+
+    last_resolved_hostname = hostname;
+    cpu->setRegister(0, REG_v0, u128::From32(1));
 }
 
 
@@ -552,6 +327,26 @@ void gethostbyname_ps2_1(bool init, DebugInterface *cpu)
         cpu->write32(func.address, OP_JR_RA);
         return;
     }
+
+	// unknown
+    // auto ticket_number = cpu->read32(cpu->getRegister(0, REG_a0));
+    // printf("ticket_number %d\n", ticket_number);
+}
+
+void AvepppGetStatus(bool init, DebugInterface* cpu) {
+    puts(__func__);
+    auto func = gdx_remaps_symbols[__func__];
+
+    if (init) {
+        // erase the function.
+        for (int i = 0; i < func.size; ++i) {
+            cpu->write8(func.address + i, 0);
+        }
+        cpu->write32(func.address, OP_JR_RA);
+        return;
+    }
+
+    cpu->setRegister(0, REG_v0, u128::From32(5)); // I don't know the magic number.
 }
 
 } // end of namespace
@@ -561,37 +356,28 @@ void gethostbyname_ps2_1(bool init, DebugInterface *cpu)
 void gdx_initialize()
 {
     gdx_remaps_name["Ave_SifCallRpc"] = Ave_SifCallRpc;
-    gdx_remaps_name["Ave_TcpInitialize"] = Ave_TcpInitialize;
-    gdx_remaps_name["Ave_TcpTerminate"] = Ave_TcpTerminate;
-    gdx_remaps_name["Ave_SetOpt"] = Ave_SetOpt;
-    gdx_remaps_name["Ave_GetOpt"] = Ave_GetOpt;
+
+    gdx_remaps_name["Ave_DnsGetTicket"] = Ave_DnsGetTicket;
+    gdx_remaps_name["Ave_DnsReleaseTicket"] = Ave_DnsReleaseTicket;
+
+    gdx_remaps_name["gethostbyname_ps2_0"] = gethostbyname_ps2_0;
+    gdx_remaps_name["gethostbyname_ps2_1"] = gethostbyname_ps2_1;
+
     gdx_remaps_name["Ave_TcpOpen"] = Ave_TcpOpen;
     gdx_remaps_name["Ave_TcpClose"] = Ave_TcpClose;
     gdx_remaps_name["Ave_TcpStat"] = Ave_TcpStat;
     gdx_remaps_name["Ave_TcpSend"] = Ave_TcpSend;
     gdx_remaps_name["Ave_TcpRecv"] = Ave_TcpRecv;
-    /*
-    gdx_remaps_name["Ave_PppInit"] = Ave_PppInit;
-    gdx_remaps_name["Ave_PppDisp"] = Ave_PppDisp;
-    gdx_remaps_name["Ave_PppStart"] = Ave_PppStart;
-    gdx_remaps_name["Ave_PppFinish"] = Ave_PppFinish;
-    //gdx_remaps_name["Ave_PppStatus"] = Ave_PppStatus;
-    gdx_remaps_name["Ave_PppGetUsbDeviceId"] = Ave_PppGetUsbDeviceId;
-    gdx_remaps_name["Ave_PppSetOption"] = Ave_PppSetOption;
-    gdx_remaps_name["Ave_PppGetOption"] = Ave_PppGetOption;
-	*/
-    gdx_remaps_name["Ave_DnsInit"] = Ave_DnsInit;
-    gdx_remaps_name["Ave_DnsDisp"] = Ave_DnsDisp;
-    gdx_remaps_name["Ave_DnsGetTicket"] = Ave_DnsGetTicket;
-    gdx_remaps_name["Ave_DnsReleaseTicket"] = Ave_DnsReleaseTicket;
-    gdx_remaps_name["Ave_DnsLookUp"] = Ave_DnsLookUp;
 
-    gdx_remaps_name["PppGetStatus"] = PppGetStatus;
-    //gdx_remaps_name["dialing_proc"] = dialing_proc;
+    gdx_remaps_name["AvepppGetStatus"] = AvepppGetStatus;
 
-    gdx_remaps_name["gethostbyname_ps2_0"] = gethostbyname_ps2_0;
-    gdx_remaps_name["gethostbyname_ps2_1"] = gethostbyname_ps2_1;
 
+    printf("Initialising Winsock");
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Failed. Error Code : %d", WSAGetLastError());
+    }
+    sock = 0;
+	FD_ZERO(&readfds);
 
     gdx_reload_breakpoints();
 }
@@ -602,14 +388,14 @@ void gdx_reload_breakpoints()
         return;
     }
 
-    auto funcs = symbolMap.GetAllSymbols(ST_FUNCTION);
-    if (funcs.empty())
-        return;
-
     auto *dlg = wxGetApp().GetDisassemblyPtr();
     if (!dlg) {
         return;
     }
+
+    auto funcs = symbolMap.GetAllSymbols(ST_FUNCTION);
+    if (funcs.empty())
+        return;
 
     printf("gdx_reload_breakpoints\n");
     auto cpu = dlg->eeTab->getCpu();
@@ -633,15 +419,10 @@ void gdx_reload_breakpoints()
         }
     }
 
-    // goto network menu
-    // cpu->write8(0x008710D0, 6);
-    // cpu->write8(0x008710D1, 0);
-
-    // replace modem_recognition -> network_battle
+    // replace modem_recognition with network_battle.
     cpu->write32(0x003c4f58, 0x0015f110);
 
-
-    // skip dialing step
+    // skip ppp dialing step
     cpu->write32(0x0035a660, 0x24030002);
 }
 
