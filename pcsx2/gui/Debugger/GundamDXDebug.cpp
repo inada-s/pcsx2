afd2c2b760a5cc6531ddbb8c7087480fcad4b2e8
Shingo INADA <mecha.is06@gmail.com>
2020-03-28 01:09:59 +0900

    fix tcp client

--- pcsx2/gui/Debugger/GundamDXDebug.cpp
+++ pcsx2/gui/Debugger/GundamDXDebug.cpp
@@ -14,66 +14,65 @@
 namespace
 {
 
-class GdxTcpClient {
-	SOCKET sock = INVALID_SOCKET;
+class GdxTcpClient
+{
+    SOCKET sock = INVALID_SOCKET;
 
 public:
-	void do_connect(const char* host, int port) {
-		SOCKET new_sock = socket(AF_INET, SOCK_STREAM, 0);
-		if (new_sock == INVALID_SOCKET) {
-			printf("socket error : %d\n", WSAGetLastError());
-			return;
-		}
+    void do_connect(const char *host, int port)
+    {
+        SOCKET new_sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (new_sock == INVALID_SOCKET) {
+            printf("socket error : %d\n", WSAGetLastError());
+            return;
+        }
 
-		auto host_entry = gethostbyname(host);
-		SOCKADDR_IN addr;
-		addr.sin_family = AF_INET;
-		addr.sin_addr = *((LPIN_ADDR)host_entry->h_addr_list[0]);
-		addr.sin_port = htons(port);
-		if (connect(new_sock, (const sockaddr*)&addr, sizeof(addr)) != NO_ERROR) {
-			printf("socket error : %d\n", WSAGetLastError());
-			return;
-		}
+        auto host_entry = gethostbyname(host);
+        SOCKADDR_IN addr;
+        addr.sin_family = AF_INET;
+        addr.sin_addr = *((LPIN_ADDR)host_entry->h_addr_list[0]);
+        addr.sin_port = htons(port);
+        if (connect(new_sock, (const sockaddr *)&addr, sizeof(addr)) != NO_ERROR) {
+            printf("socket error : %d\n", WSAGetLastError());
+            return;
+        }
 
-		/*
-		u_long mode = 1; // non-blocking
-		if (ioctlsocket(new_sock, FIONBIO, &mode) != NO_ERROR) {
-			printf("ioctlsocket error : %d\n", WSAGetLastError());
-			return;
-		}
-		*/
+        if (sock != INVALID_SOCKET) {
+            closesocket(sock);
+        }
 
-		if (sock != INVALID_SOCKET) {
-			closesocket(sock);
-		}
+        sock = new_sock;
+    }
 
-		sock = new_sock;
-	}
+    int is_connected()
+    {
+        return sock != INVALID_SOCKET;
+    }
 
-	int is_connected() {
-		return sock != INVALID_SOCKET;
-	}
+    int do_recv(char *buf, int len)
+    {
+        return recv(sock, buf, len, 0);
+    }
 
-	int do_recv(char* buf, int len) {
-		return recv(sock, buf, len, 0);
-	}
+    int do_send(const char *buf, int len)
+    {
+        return send(sock, buf, len, 0);
+    }
 
-	int do_send(const char* buf, int len) {
-		return send(sock, buf, len, 0);
-	}
+    void do_close()
+    {
+        if (sock != INVALID_SOCKET) {
+            closesocket(sock);
+            sock = INVALID_SOCKET;
+        }
+    }
 
-	void do_close() {
-		if (sock != 0) {
-			closesocket(sock);
-			sock = 0;
-		}
-	}
-
-	u32 readable_size() {
-		u_long n = 0;
-		ioctlsocket(sock, FIONREAD, &n);
-		return u32(n);
-	}
+    u32 readable_size()
+    {
+        u_long n = 0;
+        ioctlsocket(sock, FIONREAD, &n);
+        return u32(n);
+    }
 };
 
 GdxTcpClient tcp;
@@ -167,9 +166,6 @@ void dump_state()
     dump_addr("a2", r5900Debug.getRegister(0, REG_a2)._u32[0]);
     dump_addr("a3", r5900Debug.getRegister(0, REG_a3)._u32[0]);
     dump_addr("ra", r5900Debug.getRegister(0, REG_ra)._u32[0]);
-	// "ReadedDataLength", 0x00580120
-	// "LobbyDataLength", 0x00580124
-	// "HeaderReadedFlag", 0x00580128
 
     puts(">> trace");
     int d = 0;
@@ -261,13 +257,13 @@ void gdx_initialize()
 
     gdx_breakpoints.push_back(labelBP(
         "Ave_TcpOpen", []() {
-			tcp.do_connect("localhost", 3333); // dummy
+            tcp.do_connect("localhost", 3333); // dummy
         },
         true));
 
     gdx_breakpoints.push_back(labelBP(
         "Ave_TcpClose", []() {
-			tcp.do_close();
+            tcp.do_close();
         },
         true));
 
@@ -285,7 +281,7 @@ void gdx_initialize()
                 buf[i] = r5900Debug.read8(p + i);
             }
 
-			const int n = tcp.do_send(buf, len);
+            const int n = tcp.do_send(buf, len);
             if (n == len) {
                 r5900Debug.setRegister(0, REG_v0, u128::From32(n));
                 printf("-- sent --\n");
@@ -302,20 +298,14 @@ void gdx_initialize()
             const u32 len = r5900Debug.getRegister(0, REG_a2)._u32[0];
             printf("try to recv %d bytes\n", len);
 
-            if (len == 0xffffffff) {
-                puts("[WARN] Invalid length");
+            if (tcp.readable_size() < len) {
+                puts("[WARN] Not enough length");
                 r5900Debug.setRegister(0, REG_v0, u128::From32(-1));
                 return;
             }
 
-			if (tcp.readable_size() < len) {
-                puts("[WARN] Not enough length");
-                r5900Debug.setRegister(0, REG_v0, u128::From32(-1));
-                return;
-			}
-
-			char buf[1024];
-			int n = tcp.do_recv(buf, len);
+            char buf[1024];
+            int n = tcp.do_recv(buf, len);
             r5900Debug.setRegister(0, REG_v0, u128::From32(n));
 
             printf("recv %d bytes done\n", n);
@@ -332,24 +322,21 @@ void gdx_initialize()
                     printf("\n");
             }
             printf("--\n");
-
         },
         true));
 
+    // it causes too slow game but the trace is useful for lobby debugging.
     gdx_breakpoints.push_back(labelBP("SetSendCommand", nullptr, false));
-    // gdx_breakpoints.push_back(labelBP("connect_ps2", nullptr, false, true));
-    // gdx_breakpoints.push_back(labelBP("connect_ps2_check", nullptr, false, true));
-    // gdx_breakpoints.push_back(labelBP("select_ps2", nullptr, false, true));
 
     /*
+	// client sometimes check msg error code
 	gdx_breakpoints.push_back(labelBP("Check_RequestResult",[]() {
 		printf("Check_RequestResult %02x\n", r5900Debug.read8(0x00aa64e8));
 	}, false));
 	*/
 
-
     printf("Initialising Winsock");
-	WSADATA wsa;
+    WSADATA wsa;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         printf("WSAStartup failed. error : %d", WSAGetLastError());
     }
@@ -384,77 +371,74 @@ void gdx_reload_breakpoints()
         }
         gdx_breakpoints_map[bp.addr] = bp;
     }
+}
 
-    // -- patch --
+//
+// patch functions
+// The game calls these function frequently, so it contribute to too bad performance.
+//
 
+void patch_skip_modem()
+{
     // replace modem_recognition with network_battle.
     r5900Debug.write32(0x003c4f58, 0x0015f110);
 
     // skip ppp dialing step.
     r5900Debug.write32(0x0035a660, 0x24030002);
+}
 
-
-
-    //
-    // The game calls these function frequently, so it contribute to too bad performance.
-    //
-
-    // rewrite AvepppGetStatus
-    /*
-	It's too slow
-	gdx_breakpoints.push_back(labelBP("AvepppGetStatus", []() {
-		r5900Debug.setRegister(0, REG_v0, u128::From32(5)); // I don't know the magic number.
-	}, true));
-	*/
+void patch_AvepppGetStatus()
+{
     // AvepppGetStatus:
-    // jr    ra
-    // return 0x05
-	// ppp status ok (probably).
+    // return ppp status
     r5900Debug.write32(0x003584d0, OP_JR_RA);
-    r5900Debug.write32(0x003584d4, 0x24020005); // v0 = 5 
-
-
-    // rewrite Ave_TcpStat
-    // Ave_TcpStat:
-	// return -1 if no data comming
-	// set a1[0] = fd
-	// 
-	// 
-    // addiu v0, zero, $0000
-    // sw    v0, $0000(a1)
-    // jr    ra
-    // nop
-    r5900Debug.write32(0x00350990, 0x24020000); // edit this address in vsync
-    r5900Debug.write32(0x00350994, 0xaca20000);
-    r5900Debug.write32(0x00350998, OP_JR_RA);
-    // r5900Debug.write32(0x0035099c, OP_NOP);
-    r5900Debug.write32(0x0035099c, 0x24020000);
+    r5900Debug.write32(0x003584d4, 0x24020005); // v0 = 5 (ppp ok probably)
 }
 
-void rewrite_connect_ps2_check() {
-	// 00357d90: connect_ps2_check
-	// return 1 if connected
-	r5900Debug.write32(0x00357d90, 0x24020000 | tcp.is_connected());
-	r5900Debug.write32(0x00357d94, OP_JR_RA);
-	r5900Debug.write32(0x00357d98, OP_NOP);
+void patch_connect_ps2_check()
+{
+    // 00357d90: connect_ps2_check
+    // return 1 if connected
+    r5900Debug.write32(0x00357d90, 0x24020000 | tcp.is_connected());
+    r5900Debug.write32(0x00357d94, OP_JR_RA);
+    r5900Debug.write32(0x00357d98, OP_NOP);
 }
 
+void patch_TcpGetStatus()
+{
+    // 00381e70: TcpGetStatus
+    // args:
+    //	a0 : sock
+    //	a1 : status result dest
+    // returns:
+    //  v0 : -1 if no data available
+    //  a1[0] : status 0-10 (?) Note: The value is used on original connect_ps2_check.
+    //  a1[1] : how many data readable
+
+    u16 retvalue = -1;
+    u16 readable_size = 0;
+    const int n = tcp.readable_size();
+    if (0 < n) {
+        retvalue = 0;
+        readable_size = n <= 0x7fff ? n : 0x7fff;
+    }
+
+    r5900Debug.write32(0x00381e70, 0x24020000);                 // addiu v0, zero, $0000
+    r5900Debug.write32(0x00381e74, 0xaca20000);                 // sw    v0, $0000(a1)
+    r5900Debug.write32(0x00381e78, 0x24020000 | readable_size); // addiu v0, zero, $0000
+    r5900Debug.write32(0x00381e7c, 0xaca20004);                 // sw    v0, $0004(a1)
+    r5900Debug.write32(0x00381e80, 0x24020000 | retvalue);      // // sw    v0, $0000(a1)
+    r5900Debug.write32(0x00381e84, OP_JR_RA);
+    r5900Debug.write32(0x00381e88, OP_NOP);
+}
 
 void gdx_in_vsync()
 {
-    static int is_set = 0;
     if (r5900Debug.isAlive()) {
-		rewrite_connect_ps2_check();
-
-		if (tcp.is_connected()) {
-			// r5900Debug.write32(0x00350990, 0x24020c04);
-			r5900Debug.write32(0x00350990, 0x24020004);
-			// r5900Debug.write32(0x00350990, 0x24020ffff);
-		}
-		else {
-            r5900Debug.write32(0x00350990, 0x2402ffff);
-		}
-
+        patch_skip_modem();
+        patch_AvepppGetStatus();
+        patch_connect_ps2_check();
+        patch_TcpGetStatus();
     }
 }
 
